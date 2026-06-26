b600503a6b1e4c8b7da699f1f8bd7f532970b785
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2026-06-26 16:23:17 -0400

    adding job

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,12 +1,14 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
 from .core_site import CoreSiteDesign
+from .create_vlans_and_tags import CreateVLANsAndTagsDesign
 from .edge_site import EdgeDesign
 from .initial_data import InitialDesign
 from .p2p import P2PDesign
 
 __all__ = [
     "CoreSiteDesign",
+    "CreateVLANsAndTagsDesign",
     "EdgeDesign",
     "InitialDesign",
     "P2PDesign",
